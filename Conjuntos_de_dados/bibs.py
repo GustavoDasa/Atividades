e4ab2b87d9edc7c7c7cd7ac8b845a3b62d9014b4
GustavoDasa <gustavo.dasa@usp.br>
2023-12-12 10:44:59 -0300

    Create bibs.py

---
+++ Conjuntos_de_dados/bibs.py
@@ -0,0 +1,41 @@
+# Matemática
+import math
+import numpy as np
+import random as rd
+
+# Dados e Gráficos
+from statsmodels.graphics.gofplots import qqplot
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib import rcParams
+# Gráfico 3d
+from mpl_toolkits.mplot3d.axes3d import Axes3D
+
+# Estatística
+from scipy import stats
+import pylab
+import seaborn as sns
+from scipy.linalg import eigh
+from sklearn.linear_model import LinearRegression
+import sklearn as skl
+from scipy import stats
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+from statsmodels.stats.outliers_influence import OLSInfluence
+import statsmodels.formula.api as smf
+
+# Requisições
+from bs4 import BeautifulSoup as bs
+import requests
+from io import StringIO
+
+# Interações
+from ipywidgets import interact, interactive, fixed, interact_manual, Button, Layout
+import ipywidgets as widgets
+from IPython.display import display, Latex
+
+import zipfile
+
+
+X=3.145
